da503bb083383f23626d38a985f9274b7a7056c6
bernardo jose <bernardo.jose@bernardos-MacBook-Pro.local>
2025-10-07 02:51:34 +0100

    test1

--- app/routes.py
+++ app/routes.py
@@ -7,10 +7,7 @@ bp = Blueprint('main', __name__)
 @bp.route('/')
 def index():
     return render_template('index.html')
-
-@bp.route('/health')
-def health():
-    return jsonify({"status": "healthy"})
+    
 
 @bp.route('/tasks', methods=['GET'])
 def get_tasks():
